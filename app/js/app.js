b61628efe93b9a72cef769687d269558495adb08
Max <max_lisyanskiy@mail.ru>
2020-12-04 17:08:24 +0300

    Mobile first changes 04.12

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     //Воспроизведение музыки вначале и по клику
-    var track = document.querySelector('audio');
-        track.volume = 0.3;
-        track.play();
+    var track = document.querySelector('#myaudio');
+        track.volume = 0.1;
       
     $('.main-top__sound-icon').on('click', function(){
-    var track = document.querySelector('audio');
+    var track = document.querySelector('#myaudio');
         if (track.paused) {
         track.play();
         $(this).attr('src', 'images/dest/SoundOn.png');
@@ -29,20 +28,28 @@ document.addEventListener("DOMContentLoaded", function() {
         $('#main-middle').addClass('fadeOutDown').fadeOut();
     }, 14000);
 
-    //Появление main-secondMiddle
+    //Появление main-secondMiddle__text
     setTimeout(function() {
         $('.main-secondMiddle__text').fadeIn().addClass('fadeInDown');
     }, 15000);
 
-    //Появление main-secondMiddle
+    //Появление btn-orange
     setTimeout(function() {
         $('.btn-orange').css('display', 'block').addClass('fadeInUp');
     }, 16000);
 
-    //Появление main-secondMiddle
-    setTimeout(function() {
-        $('#btn').css('display', 'block').addClass('fadeInUp');
-    }, 16000);
+    //Появление btn
+    function windowSize(){
+        if ($(window).width() >= '500'){
+            setTimeout(function() {
+                $('#btn').css('display', 'block').addClass('fadeInUp');
+            }, 15000);
+        } else {
+            $('#btn').css('display', 'block');
+        }
+    }
+
+    $(window).on('load resize',windowSize);
 });
 
 
